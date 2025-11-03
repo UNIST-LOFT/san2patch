04eb5fca953481c0fe1242b78bda751eda6e1d9d
hsh814 <hsh814@naver.com>
2025-11-03 01:44:36 +0000

    Add template

--- benchmarks/final/final-test/collect.py
+++ benchmarks/final/final-test/collect.py
@@ -1,6 +1,30 @@
 import os
+import json
+
+for bug_id in os.listdir("vuln"):
+    with open(f"vuln/{bug_id}", "r") as f:
+        data = json.load(f)
+    subject = data["subject"]
+    bid = data["bug_id"]
+    print(f"Processing {subject} {bid}")
+    if "predefined_fix_locations" in data:
+        locs = data["predefined_fix_locations"]
+        file_path = locs[0]["file_name"]
+        start_line = locs[0]["start_line"]
+        end_line = locs[0]["end_line"]
+        fix_line = locs[0]["fix_line"]
+        with open(os.path.join("repo", f"{subject}_{bid}", file_path), "r") as f:
+            lines = f.readlines()
+        with open(os.path.join("patch", f"{bid}.template"), "w") as f:
+            for i in range(start_line, end_line + 1):
+                line_content = lines[i - 1].rstrip('\n')
+                if i == fix_line - 1:
+                    f.write(f"{i:4d} | {line_content} // PATCH LOCATION")
+                else:
+                    f.write(f"{i:4d} | {line_content}\n")
 
 
+exit(0)
 exps = [
     "gen_diff_usenix_tot_vulnloc_semdiff",
 ]
